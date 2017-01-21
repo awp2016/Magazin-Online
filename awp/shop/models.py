30c4cfe0d86a131c04aa182b6ca04017a3d2a0d7
Andrei-Chi <andrei.chihalau@yahoo.com>
2017-01-21 13:27:16 +0200

    update baza de date

--- awp/shop/models.py
+++ awp/shop/models.py
@@ -18,3 +18,20 @@ class Product(models.Model):
   unit = models.CharField(max_length=5, default='Units')
   def __unicode__(self):
     return 'Name {}, Category {}, Description {}, Price {}, Stock_number {}'.format(self.product_name, self.category, self.description, self.price, self.stock_number)
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birthday = models.DateField(null=True)
+    address = models.CharField(max_length=150)
+    avatar = models.ImageField(null=True, upload_to='images')
+    user = models.OneToOneField(User, primary_key=True, related_name='profile')
+
+class MyShoppingCart(models.Model):
+  client= models.OneToOneField(Client , primary_key=True, related_name='clients_cart')
+
+class ShoppingCartItem(models.Model):
+  cart= models.ForeignKey(MyShoppingCart)
+  product= models.ForeignKey(Product)
+
+  
